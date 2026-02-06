8127152c56bd68f160ab6bddf3223740331a4b15
Noor <zcbmq14@gmail.com>
2026-02-07 00:25:31 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <h1 className="mt-8 text-3xl font-bold text-gray-900">Me?</h1>
+
+      <p className="mt-4 text-lg leading-relaxed text-gray-700">
+        I'm a software engineering and web developer student. I'm incredibly
+        curious to learn everything I can, but unfortunately, I know it's
+        impossible. But of course, I'll keep learning.
+      </p>
+
+      <p className="mt-6 text-lg leading-relaxed text-gray-700">
+        And honestly, I'm still discovering more about myself every day. And you
+        know what? I love writing, whether it's programming or just regular
+        writing.
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="bg-linear-to-r from-blue-600 to-purple-500 h-16 flex items-center justify-center">
+        <h1 className="text-white text-4xl font-bold">mnoNoor</h1>
+      </div>
+    </header>
+  );
+}
